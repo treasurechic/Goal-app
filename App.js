5a58fdce48442b5db4a002001425ed7204c7be17
Treasurechic <babychic140@gmail.com>
2021-08-04 17:15:08 +0100

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState();
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (text) => {
+    setEnteredGoal(text);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals([...courseGoals, enteredGoal]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+        />
+        <Button title="ADD" onPress={addGoalHandler} />
+      </View>
+      <ScrollView style={{backgroundColor:'red', flex:1}}>
+        {courseGoals.map((goal, index) => (
+          <View style={styles.listItem} key={index}>
+            <Text>{goal}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: { padding: 50, flex:1 },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    width: "80%",
+    borderColor: "#484848",
+    borderWidth: 1,
+    padding: 10,
+  },
+  listItem: {
+    backgroundColor: "#ccc",
+    padding: 10,
+    marginVertical: 10,
+    borderWidth: 1,
+    borderColor: "black",
   },
 });
